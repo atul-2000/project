aad0e1b3c0031c611c0e82afcf62d522cdc03375
atul-2000 <atularohi2017@gmail.com>
2021-01-04 20:31:28 +0530

    updated one

--- Bank.java
+++ Bank.java
@@ -1,26 +1,14 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Bank {
+	   
+	public static Scanner sc = new Scanner(System.in);
+	public static Account acc = null;
 
-	public static void main(String[] args) {
-		Account acc = null;
-		Scanner sc = new Scanner(System.in);
-		
-		while(true)
-		{
-		  System.out.println("1. Create Account");
-		  System.out.println("2. Deposit");
-		  System.out.println("3. Withdraw");
-		  System.out.println("4. Details");
-		  System.out.println("5. Exit");
-		  
-		  System.out.print("enter an option: ");
-		  int opt = sc.nextInt();
-		  
-		  switch(opt)
-		  {
-		  case 1: //create Account
-			  if(acc == null) {
+       public static void create_account(){
+		         //Account acc = null;
+                       if(acc == null) {
 				  System.out.print("enter account number: ");
 				  long acno = sc.nextLong();
 				  sc.nextLine();
@@ -38,43 +26,73 @@ public class Bank {
 			  else {
 				  System.out.println("FYI: Account is already Created!");
 			  }
-			  break;
-		  case 2: //deposit
-			  if(acc != null) {
-				  System.out.print("enter amount to deposit: ");
-				  float amt = sc.nextFloat();
-				  acc.deposit(amt);
-			  }
-			  else {
-				  System.out.println("FYI : Create Account First!");
-			  }
-			  break;		
-		  case 3: //withdraw
-			  if(acc != null) {
-				  System.out.print("enter amount to withdraw: ");
-				  float amt = sc.nextFloat();
-				  acc.withdraw(amt);
-			  }
-			  else {
-				  System.out.println("FYI : Create Account First!");
-			  }			  
-			  break;		
-		  case 4: //details
-			  if(acc != null) {				  
-				  acc.getBalance();
-			  }
-			  else {
-				  System.out.println("FYI : Create Account First!");
-			  }			  
-			  break;		
-		  case 5: //terminate application
-			  System.out.println("--- BYE BYE ---");
-			  System.exit(0);
-			  break;			  
-		  default:
-			  System.out.println("Invalid Option.. Try Again..");
-		  }//switch
-		  System.out.println("--------------------------------");
+	   }
+	   
+	   public static void deposite(){
+		if(acc != null) {
+			System.out.print("enter amount to deposit: ");
+			float amt = sc.nextFloat();
+			acc.deposit(amt);
+		}
+		else {
+			System.out.println("FYI : Create Account First!");
+		}
+
+	   }
+
+	   public static void withdraw(){
+		if(acc != null) {
+			System.out.print("enter amount to withdraw: ");
+			float amt = sc.nextFloat();
+			acc.withdraw(amt);
+		}
+		else {
+			System.out.println("FYI : Create Account First!");
+		}	
+	   }
+
+	   public static void details(){
+		if(acc != null) {				  
+			acc.getBalance();
+		}
+		else {
+			System.out.println("FYI : Create Account First!");
+		}	
+	   }
+
+	   public static void terminate(){
+		System.out.println("--- BYE BYE ---");
+		System.exit(0);
+	   }
+       
+	public static void main(String[] args) {
+				
+		Map<Integer, Runnable> commands = new HashMap<>();
+		commands.put(1, () -> create_account());
+		commands.put(2, () -> deposite());
+		commands.put(3, () -> withdraw());
+		commands.put(4, () -> details());
+		commands.put(5, () -> terminate());
+		
+		while(true)
+		{
+		  System.out.println("a. Create Account");
+		  System.out.println("2. Deposit");
+		  System.out.println("3. Withdraw");
+		  System.out.println("4. Details");
+		  System.out.println("5. Exit");
+		  
+		  System.out.print("enter an option: ");
+		 
+            int key = sc.nextInt();
+
+            // Run selected command
+            if (commands.containsKey(key))
+                commands.get(key).run();
+		  
+		    if(key==5){
+				break;
+			}
 		}//while
 	}
 
